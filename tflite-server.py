e2f72496889e8702ee165c9d72639febf75a08bb
Robin Cole <robmarkcole@gmail.com>
2019-11-23 07:19:40 +0000

    Update gitignore

--- tflite-server.py
+++ tflite-server.py
@@ -19,12 +19,14 @@ logging.basicConfig(filename='tflite-server.log', level=logging.DEBUG, format=LO
 
 interpreter = None
 labels = None
-labels_file = None
 input_details = None
 output_details = None
 model_height = None
 model_width = None
 floating_model = None
+
+input_mean = 127.5
+input_std = 127.5
 
 ROOT_URL = "/v1/vision/detection"
 
@@ -32,17 +34,6 @@ ROOT_URL = "/v1/vision/detection"
 def load_labels(filename):
   with open(filename, 'r') as f:
     return [line.strip() for line in f.readlines()]
-
-# Function to read labels from text files.
-def ReadLabelFile(file_path):
-    with open(file_path, "r", encoding="utf-8") as f:
-        lines = f.readlines()
-        ret = {}
-        for line in lines:
-            pair = line.strip().split(maxsplit=1)
-            ret[int(pair[0])] = pair[1].strip()
-    return ret
-
 
 @app.route("/")
 def info():
@@ -56,41 +47,33 @@ def predict():
 
     if not flask.request.method == "POST":
         return
-    try:
-        if flask.request.files.get("image"):
-            image_file = flask.request.files["image"]
-            image_bytes = image_file.read()
-            img = Image.open(io.BytesIO(image_bytes)).resize((model_width, model_height))
 
-            # add N dim
-            input_data = np.expand_dims(img, axis=0)
+    if flask.request.files.get("image"):
+        image_file = flask.request.files["image"]
+        image_bytes = image_file.read()
+        img = Image.open(io.BytesIO(image_bytes)).resize((model_width, model_height))
 
+        # add N dim
+        input_data = np.expand_dims(img, axis=0)
+
+        if floating_model:
+            input_data = (np.float32(input_data) - args.input_mean) / args.input_std
+
+        interpreter.set_tensor(input_details[0]['index'], input_data)
+        interpreter.invoke()
+        output_data = interpreter.get_tensor(output_details[0]['index'])
+        results = np.squeeze(output_data)
+
+        top_k = results.argsort()[-5:][::-1]
+        print(top_k)
+        
+        for i in top_k:
             if floating_model:
-                input_mean = 127.5
-                input_std = 127.5
-                input_data = (np.float32(input_data) - args.input_mean) / args.input_std
+                print('{:08.6f}: {}'.format(float(results[i]), labels[i]))
+            else:
+                print('{:08.6f}: {}'.format(float(results[i] / 255.0), labels[i]))
 
-            interpreter.set_tensor(input_details[0]['index'], input_data)
-
-            interpreter.invoke()
-
-            output_data = interpreter.get_tensor(output_details[0]['index'])
-            results = np.squeeze(output_data)
-            # Run inference.
-            top_k = results.argsort()[-5:][::-1]
-            labels = load_labels(labels_file)
-            for i in top_k:
-                if floating_model:
-                    print('{:08.6f}: {}'.format(float(results[i]), labels[i]))
-                else:
-                    print('{:08.6f}: {}'.format(float(results[i] / 255.0), labels[i]))
-
-            return flask.jsonify(data)
-
-    except Exception as exc:
-        data['error'] = str(exc)
         return flask.jsonify(data)
-
 
 
 if __name__ == "__main__":
@@ -129,5 +112,5 @@ if __name__ == "__main__":
     floating_model = input_details[0]['dtype'] == np.float32
 
     print("\n Loaded model : {}".format(model_file))
-    labels = ReadLabelFile(labels_file)
+    labels = load_labels(labels_file)
     app.run(host="0.0.0.0", debug=True, port=args.port)
