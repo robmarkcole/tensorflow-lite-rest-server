5b2fcd9aabe78bc96aaf804e553283d72547a5fd
Robin Cole <robmarkcole@gmail.com>
2020-06-09 05:33:07 +0100

    initial face

--- tflite-server.py
+++ tflite-server.py
@@ -32,6 +32,45 @@ def info():
     )
 
 
+@app.route(FACE_DETECTION_URL, methods=["POST"])
+def predict():
+    data = {"success": False}
+    if not flask.request.method == "POST":
+        return
+
+    if flask.request.files.get("image"):
+        # Open image and get bytes and size
+        image_file = flask.request.files["image"]
+        image_bytes = image_file.read()
+        image = Image.open(io.BytesIO(image_bytes))  # A PIL image
+        image_width = image.size[0]
+        image_height = image.size[1]
+
+        # Format data and send to interpreter
+        resized_image = image.resize((face_input_width, face_input_height))
+        input_data = np.expand_dims(resized_image, axis=0)
+        face_interpreter.set_tensor(face_input_details[0]["index"], input_data)
+
+        # Process image and get predictions
+        face_interpreter.invoke()
+        boxes = face_interpreter.get_tensor(face_output_details[0]["index"])[0]
+        scores = face_interpreter.get_tensor(face_output_details[2]["index"])[0]
+
+        faces = []
+        for i in range(len(scores)):
+            single_face = {}
+            single_face["confidence"] = float(scores[i])
+            single_face["y_min"] = int(float(boxes[i][0]) * image_height)
+            single_face["x_min"] = int(float(boxes[i][1]) * image_width)
+            single_face["y_max"] = int(float(boxes[i][2]) * image_height)
+            single_face["x_max"] = int(float(boxes[i][3]) * image_width)
+            faces.append(single_face)
+
+        data["predictions"] = faces
+        data["success"] = True
+        return flask.jsonify(data)
+
+
 @app.route(OBJ_DETECTION_URL, methods=["POST"])
 def predict():
     data = {"success": False}
@@ -42,7 +81,7 @@ def predict():
         # Open image and get bytes and size
         image_file = flask.request.files["image"]
         image_bytes = image_file.read()
-        image = Image.open(io.BytesIO(image_bytes))
+        image = Image.open(io.BytesIO(image_bytes))  # A PIL image
         image_width = image.size[0]
         image_height = image.size[1]
 
@@ -86,5 +125,13 @@ if __name__ == "__main__":
     obj_input_height = obj_input_details[0]["shape"][1]
     obj_input_width = obj_input_details[0]["shape"][2]
     obj_labels = read_coco_labels(OBJ_LABELS)
+
+    # Setup face detection
+    face_interpreter = tflite.Interpreter(model_path=FACE_MODEL)
+    face_interpreter.allocate_tensors()
+    face_input_details = face_interpreter.get_input_details()
+    face_output_details = face_interpreter.get_output_details()
+    face_input_height = 320
+    face_input_width = 320
 
     app.run(host="0.0.0.0", debug=True, port=args.port)
