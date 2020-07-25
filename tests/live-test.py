624f711529adfdf944101f9860218c854be7ace6
Robin Cole <robmarkcole@gmail.com>
2020-07-25 07:24:30 +0100

    update live-test

--- tests/live-test.py
+++ tests/live-test.py
@@ -1,9 +1,28 @@
+"""Perform processing of test images."""
+import pprint
+
 import requests
 
-API_URL = "http://localhost:5000/v1/vision/detection"
+print("-----------------------------")
+OBJ_DETECTION_URL = "http://localhost:5000/v1/vision/detection"
+OBJ_DETECTION_TEST_IMAGE = "people_car.jpg"
+image_data = open(OBJ_DETECTION_TEST_IMAGE, "rb").read()
+response = requests.post(OBJ_DETECTION_URL, files={"image": image_data}).json()
+print(OBJ_DETECTION_TEST_IMAGE)
+pprint.pprint(response)
 
-image_data = open("people_car.jpg", "rb").read()
+print("-----------------------------")
+FACE_DETECTION_URL = "http://localhost:5000/v1/vision/face"
+FACE_DETECTION_TEST_IMAGE = "faces.jpg"
+image_data = open(FACE_DETECTION_TEST_IMAGE, "rb").read()
+response = requests.post(FACE_DETECTION_URL, files={"image": image_data}).json()
+print(FACE_DETECTION_TEST_IMAGE)
+pprint.pprint(response)
 
-response = requests.post(API_URL, files={"image": image_data}).json()
-
-print(response)
+print("-----------------------------")
+SCENE_URL = "http://localhost:5000/v1/vision/scene"
+SCENE_TEST_IMAGE = "dog.jpg"
+image_data = open(SCENE_TEST_IMAGE, "rb").read()
+response = requests.post(SCENE_URL, files={"image": image_data}).json()
+print(SCENE_TEST_IMAGE)
+pprint.pprint(response)
