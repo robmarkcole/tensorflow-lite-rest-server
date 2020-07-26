85acee80416026e724b41a664365b7c83fc71b0c
Robin Cole <robmarkcole@gmail.com>
2020-07-26 07:43:31 +0100

    add flask

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # tensorflow-lite-rest-server
-Expose tensorflow-lite models via a rest API. Currently object, face & scene detection is supported. Can be hosted on any of the common platforms including RPi, linux desktop, Mac and Windows. A service can be used to have the server run automatically on an RPi.
+Expose tensorflow-lite models via a rest API, and currently object, face & scene detection is supported. Can be hosted on any of the common platforms including RPi, linux desktop, Mac and Windows.
 
 ## Setup
-In this process we create a virtual environment (venv), then install tensorflow-lite [as per these instructions](https://www.tensorflow.org/lite/guide/python) which is platform specific, and finally install the remaining requirements. **Note** on an RPi (only) it is necessary to system wide install pip3, numpy, pillow.
+In this process we create a virtual environment (venv), then install tensorflow-lite [as per these instructions](https://www.tensorflow.org/lite/guide/python) which is platform specific, and finally install the remaining requirements. Note on an RPi (only) it is necessary to manually install pip3, numpy, pillow.
 
 All instructions for mac:
 ```
@@ -13,40 +13,38 @@ pip3 install -r requirements.txt
 ```
 
 ## Models
-For convenience a couple of models are included in this repo and used by default. A description of each model is included in its directory. Additional models are available [here](https://github.com/google-coral/edgetpu/tree/master/test_data).
-
-If you want to create custom models, there is the easy way, and the longer but more flexible way. The easy way is to use [teachablemachine](https://teachablemachine.withgoogle.com/train/image), which I have done in this repo for the dogs-vs-cats model. The teachablemachine service is limited to image classification but is very straightforward to use. The longer way allows you to use any neural network architecture to produce a tensorflow model, which you then convert to am optimized tflite model. An example of this approach is described in [this article](https://towardsdatascience.com/inferences-from-a-tf-lite-model-transfer-learning-on-a-pre-trained-model-e16e7c5f0ee6), or jump straight [to the code](https://github.com/arshren/TFLite/blob/master/Transfer%20Learning%20with%20TFLite-Copy1.ipynb).
+For convenience a couple of models are included in this repo and used by default. A description of each model is included in its directory. Additional models are available [here](https://github.com/google-coral/edgetpu/tree/master/test_data)
 
 ## Usage
-Start the tflite-server on port 5000 :
+Start the server on port 5000 (default is port 5000):
 ```
-(venv) $ uvicorn tflite-server:app --reload --port 5000 --host 0.0.0.0
+(venv) $ python3 tflite-server.py --port 5000
 ```
 
-You can check that the tflite-server is running by visiting `http://ip:5000/` from any machine, where `ip` is the ip address of the host (`localhost` if querying from the same machine). The docs can be viewed at `http://localhost:5000/docs`
+You can check that the app is running by visiting `http://ip:5000/` from any machine, where `ip` is the ip address of the host (`localhost` if querying from the same machine).
 
 Post an image to detecting objects via cURL:
 ```
-curl -X POST "http://localhost:5000/v1/vision/detection" -H  "accept: application/json" -H  "Content-Type: multipart/form-data" -F "image=@tests/people_car.jpg;type=image/jpeg"
+curl -X POST -F image=@tests/people_car.jpg 'http://localhost:5000/v1/vision/detection'
 ```
 Which should return:
 ```
 {
   "predictions": [
     {
-      "confidence": 0.93359375,
-      "label": "car",
-      "x_max": 619,
-      "x_min": 302,
-      "y_max": 348,
+      "confidence": 0.93359375, 
+      "label": "car", 
+      "x_max": 619, 
+      "x_min": 302, 
+      "y_max": 348, 
       "y_min": 120
-    },
+    }, 
     {
-      "confidence": 0.7890625,
-      "label": "person",
-      "x_max": 363,
-      "x_min": 275,
-      "y_max": 323,
+      "confidence": 0.7890625, 
+      "label": "person", 
+      "x_max": 363, 
+      "x_min": 275, 
+      "y_max": 323, 
       "y_min": 126
     },
 .
@@ -54,55 +52,30 @@ Which should return:
 .
 'success': True}
 ```
-An example request using the python requests package is in `tests/live-test.py`
 
-## Add tflite-server as a service
-You can run tflite-server as a [service](https://www.raspberrypi.org/documentation/linux/usage/systemd.md), which means tflite-server will automatically start on RPi boot, and can be easily started & stopped. Create the service file in the appropriate location on the RPi using: ```sudo nano /etc/systemd/system/tflite-server.service```
-
-Entering the following (adapted for your `tflite-server.py` file location and args):
+To detect faces:
 ```
-[Unit]
-Description=Rest API exposing tensorflow lite models
-After=network.target
-
-[Service]
-ExecStart=/home/pi/.local/bin/uvicorn tflite-server:app --reload --port 5000 --host 0.0.0.0
-WorkingDirectory=/home/pi/github/tensorflow-lite-rest-server
-StandardOutput=inherit
-StandardError=inherit
-Restart=always
-User=pi
-
-[Install]
-WantedBy=multi-user.target
+curl -X POST -F image=@tests/faces.jpg 'http://localhost:5000/v1/vision/face'
 ```
 
-Once this file has been created you can to start the service using:
-```sudo systemctl start tflite-server.service```
-
-View the status and logs with:
-```sudo systemctl status tflite-server.service```
-
-Stop the service with:
-```sudo systemctl stop tflite-server.service```
-
-Restart the service with:
-```sudo systemctl restart tflite-server.service```
-
-You can have the service auto-start on rpi boot by using:
-```sudo systemctl enable tflite-server.service```
-
-You can disable auto-start using:
-```sudo systemctl disable tflite-server.service```
+To run the scene:
+```
+curl -X POST -F image=@tests/cat.jpg 'http://localhost:5000/v1/vision/scene'
+or
+curl -X POST -F image=@tests/dog.jpg 'http://localhost:5000/v1/vision/scene'
+```
 
 ## Deepstack, Home Assistant & UI
 This API can be used as a drop in replacement for [deepstack object detection](https://github.com/robmarkcole/HASS-Deepstack-object) and [deepstack face detection](https://github.com/robmarkcole/HASS-Deepstack-face) (configuring `detect_only: True`) in Home Assistant. I also created a UI for viewing the predictions of the object detection model [here](https://github.com/robmarkcole/deepstack-ui).
 
 ## Development
-I am developing on a mac/pi4 using VScode, with black formatting & line length 120.
+I am developing on a mac/pi4 using VScode. On mac use a venv, on pi install system wide.
 
 * First time only, create venv: `python3.7 -m venv venv`
 * Activate venv: `source venv/bin/activate`
 * Install the dev requirements: `pip3 install -r requirements.txt` & `pip3 install -r requirements-dev.txt`
 * Sort requirements: `venv/bin/isort tflite-server.py`
 * Black format: `venv/bin/black tflite-server.py`
+* Run the `usage.ipynb` notebook: `venv/bin/jupyter notebook`
+
+Unfortunately appears black is not supported on pi4 yet.
--- tflite-server.py
+++ tflite-server.py
@@ -1,90 +1,87 @@
 """
-Expose tflite models via a rest API.
+Expose a tflite models via a rest API.
 """
+import argparse
 import io
+import logging
 import sys
 
+import flask
 import numpy as np
 import tflite_runtime.interpreter as tflite
-from fastapi import FastAPI, File, HTTPException, UploadFile
 from PIL import Image
 
-from helpers import classify_image, read_labels, set_input_tensor
+from helpers import read_labels, set_input_tensor, classify_image
 
-app = FastAPI()
+app = flask.Flask(__name__)
 
-# Settings
+LOGFORMAT = "%(asctime)s %(levelname)s %(name)s : %(message)s"
+logging.basicConfig(
+    # filename="tflite-server.log", # select filename or stream
+    stream=sys.stdout,
+    level=logging.DEBUG,
+    format=LOGFORMAT,
+)
+
 MIN_CONFIDENCE = 0.1  # The absolute lowest confidence for a detection.
-# URL
+
 FACE_DETECTION_URL = "/v1/vision/face"
-OBJ_DETECTION_URL = "/v1/vision/detection"
-SCENE_URL = "/v1/vision/scene"
-# Models and labels
 FACE_MODEL = "models/face_detection/mobilenet_ssd_v2_face/mobilenet_ssd_v2_face_quant_postprocess.tflite"
+
+OBJ_DETECTION_URL = "/v1/vision/detection"
 OBJ_MODEL = "models/object_detection/mobilenet_ssd_v2_coco/mobilenet_ssd_v2_coco_quant_postprocess.tflite"
 OBJ_LABELS = "models/object_detection/mobilenet_ssd_v2_coco/coco_labels.txt"
+
+SCENE_URL = "/v1/vision/scene"
 SCENE_MODEL = "models/classification/dogs-vs-cats/model.tflite"
 SCENE_LABELS = "models/classification/dogs-vs-cats/labels.txt"
 
-# Setup object detection
-obj_interpreter = tflite.Interpreter(model_path=OBJ_MODEL)
-obj_interpreter.allocate_tensors()
-obj_input_details = obj_interpreter.get_input_details()
-obj_output_details = obj_interpreter.get_output_details()
-obj_input_height = obj_input_details[0]["shape"][1]
-obj_input_width = obj_input_details[0]["shape"][2]
-obj_labels = read_labels(OBJ_LABELS)
 
-# Setup face detection
-face_interpreter = tflite.Interpreter(model_path=FACE_MODEL)
-face_interpreter.allocate_tensors()
-face_input_details = face_interpreter.get_input_details()
-face_output_details = face_interpreter.get_output_details()
-face_input_height = face_input_details[0]["shape"][1]
-face_input_width = face_input_details[0]["shape"][2]
-
-# Setup face detection
-scene_interpreter = tflite.Interpreter(model_path=SCENE_MODEL)
-scene_interpreter.allocate_tensors()
-scene_input_details = scene_interpreter.get_input_details()
-scene_output_details = scene_interpreter.get_output_details()
-scene_input_height = scene_input_details[0]["shape"][1]
-scene_input_width = scene_input_details[0]["shape"][2]
-scene_labels = read_labels(SCENE_LABELS)
+@app.route("/")
+def info():
+    return f"""
+        Object detection model: {OBJ_MODEL.split("/")[-2]} \n
+        Face detection model: {FACE_MODEL.split("/")[-2]} \n
+        Scene model: {SCENE_MODEL.split("/")[-2]} \n
+        """.replace(
+        "\n", "<br>"
+    )
 
 
-@app.get("/")
-async def info():
-    return """tflite-server docs at ip:port/docs"""
+@app.route(FACE_DETECTION_URL, methods=["POST"])
+def predict_face():
+    data = {"success": False}
+    if not flask.request.method == "POST":
+        return
 
-
-@app.post(FACE_DETECTION_URL)
-async def predict_face(image: UploadFile = File(...)):
-    try:
-        contents = await image.read()
-        image = Image.open(io.BytesIO(contents))
+    if flask.request.files.get("image"):
+        # Open image and get bytes and size
+        image_file = flask.request.files["image"]
+        image_bytes = image_file.read()
+        image = Image.open(io.BytesIO(image_bytes))  # A PIL image
         image_width = image.size[0]
         image_height = image.size[1]
 
         # Format data and send to interpreter
-        resized_image = image.resize((face_input_width, face_input_height), Image.ANTIALIAS)
+        resized_image = image.resize((face_input_width, face_input_height))
         input_data = np.expand_dims(resized_image, axis=0)
         face_interpreter.set_tensor(face_input_details[0]["index"], input_data)
 
         # Process image and get predictions
         face_interpreter.invoke()
         boxes = face_interpreter.get_tensor(face_output_details[0]["index"])[0]
-        classes = face_interpreter.get_tensor(face_output_details[1]["index"])[0]
+        classes = face_interpreter.get_tensor(face_output_details[1]["index"])[
+            0
+        ]
         scores = face_interpreter.get_tensor(face_output_details[2]["index"])[0]
 
-        data = {}
         faces = []
         for i in range(len(scores)):
             if not classes[i] == 0:  # Face
                 continue
             single_face = {}
-            single_face["userid"] = "unknown"
             single_face["confidence"] = float(scores[i])
+            single_face["userid"] = "unknown"
             single_face["y_min"] = int(float(boxes[i][0]) * image_height)
             single_face["x_min"] = int(float(boxes[i][1]) * image_width)
             single_face["y_max"] = int(float(boxes[i][2]) * image_height)
@@ -95,22 +92,25 @@ async def predict_face(image: UploadFile = File(...)):
 
         data["predictions"] = faces
         data["success"] = True
-        return data
-    except:
-        e = sys.exc_info()[1]
-        raise HTTPException(status_code=500, detail=str(e))
+        return flask.jsonify(data)
 
 
-@app.post(OBJ_DETECTION_URL)
-async def predict_object(image: UploadFile = File(...)):
-    try:
-        contents = await image.read()
-        image = Image.open(io.BytesIO(contents))
+@app.route(OBJ_DETECTION_URL, methods=["POST"])
+def predict_object():
+    data = {"success": False}
+    if not flask.request.method == "POST":
+        return
+
+    if flask.request.files.get("image"):
+        # Open image and get bytes and size
+        image_file = flask.request.files["image"]
+        image_bytes = image_file.read()
+        image = Image.open(io.BytesIO(image_bytes))  # A PIL image
         image_width = image.size[0]
         image_height = image.size[1]
 
         # Format data and send to interpreter
-        resized_image = image.resize((obj_input_width, obj_input_height), Image.ANTIALIAS)
+        resized_image = image.resize((obj_input_width, obj_input_height))
         input_data = np.expand_dims(resized_image, axis=0)
         obj_interpreter.set_tensor(obj_input_details[0]["index"], input_data)
 
@@ -120,7 +120,6 @@ async def predict_object(image: UploadFile = File(...)):
         classes = obj_interpreter.get_tensor(obj_output_details[1]["index"])[0]
         scores = obj_interpreter.get_tensor(obj_output_details[2]["index"])[0]
 
-        data = {}
         objects = []
         for i in range(len(scores)):
             single_object = {}
@@ -137,25 +136,68 @@ async def predict_object(image: UploadFile = File(...)):
 
         data["predictions"] = objects
         data["success"] = True
-        return data
-    except:
-        e = sys.exc_info()[1]
-        raise HTTPException(status_code=500, detail=str(e))
+        return flask.jsonify(data)
 
 
-@app.post(SCENE_URL)
-async def predict_scene(image: UploadFile = File(...)):
-    try:
-        contents = await image.read()
-        image = Image.open(io.BytesIO(contents))
-        resized_image = image.resize((scene_input_width, scene_input_height), Image.ANTIALIAS)
+@app.route(SCENE_URL, methods=["POST"])
+def predict_scene():
+    data = {"success": False}
+    if not flask.request.method == "POST":
+        return
+
+    if flask.request.files.get("image"):
+        # Open image and get bytes and size
+        image_file = flask.request.files["image"]
+        image_bytes = image_file.read()
+        image = Image.open(io.BytesIO(image_bytes))  # A PIL image
+        # Format data and send to interpreter
+        resized_image = image.resize(
+            (scene_input_width, scene_input_height), Image.ANTIALIAS
+        )
         results = classify_image(scene_interpreter, image=resized_image)
+
+        print(
+            f"results[0]: {results[0]}", file=sys.stderr,
+        )
         label_id, prob = results[0]
-        data = {}
+
         data["label"] = scene_labels[label_id]
         data["confidence"] = prob
         data["success"] = True
-        return data
-    except:
-        e = sys.exc_info()[1]
-        raise HTTPException(status_code=500, detail=str(e))
+        return flask.jsonify(data)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Flask app exposing tflite models"
+    )
+    parser.add_argument("--port", default=5000, type=int, help="port number")
+    args = parser.parse_args()
+
+    # Setup object detection
+    obj_interpreter = tflite.Interpreter(model_path=OBJ_MODEL)
+    obj_interpreter.allocate_tensors()
+    obj_input_details = obj_interpreter.get_input_details()
+    obj_output_details = obj_interpreter.get_output_details()
+    obj_input_height = obj_input_details[0]["shape"][1]
+    obj_input_width = obj_input_details[0]["shape"][2]
+    obj_labels = read_labels(OBJ_LABELS)
+
+    # Setup face detection
+    face_interpreter = tflite.Interpreter(model_path=FACE_MODEL)
+    face_interpreter.allocate_tensors()
+    face_input_details = face_interpreter.get_input_details()
+    face_output_details = face_interpreter.get_output_details()
+    face_input_height = face_input_details[0]["shape"][1]  # 320
+    face_input_width = face_input_details[0]["shape"][2]  # 320
+
+    # Setup face detection
+    scene_interpreter = tflite.Interpreter(model_path=SCENE_MODEL)
+    scene_interpreter.allocate_tensors()
+    scene_input_details = scene_interpreter.get_input_details()
+    scene_output_details = scene_interpreter.get_output_details()
+    scene_input_height = scene_input_details[0]["shape"][1]
+    scene_input_width = scene_input_details[0]["shape"][2]
+    scene_labels = read_labels(SCENE_LABELS)
+
+    app.run(host="0.0.0.0", debug=True, port=args.port)
