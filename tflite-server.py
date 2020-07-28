44b80f2ac653575cd89bdc68bfa3d3d58ffd306b
Robin Cole <robmarkcole@gmail.com>
2020-07-28 07:40:47 +0100

    Update tflite-server.py

--- tflite-server.py
+++ tflite-server.py
@@ -26,13 +26,12 @@ logging.basicConfig(
 MIN_CONFIDENCE = 0.1  # The absolute lowest confidence for a detection.
 
 FACE_DETECTION_URL = "/v1/vision/face"
-FACE_MODEL = "models/face_detection/mobilenet_ssd_v2_face/mobilenet_ssd_v2_face_quant_postprocess.tflite"
-
 OBJ_DETECTION_URL = "/v1/vision/detection"
+SCENE_URL = "/v1/vision/scene"
+
+FACE_MODEL = "models/face_detection/mobilenet_ssd_v2_face/mobilenet_ssd_v2_face_quant_postprocess.tflite"
 OBJ_MODEL = "models/object_detection/mobilenet_ssd_v2_coco/mobilenet_ssd_v2_coco_quant_postprocess.tflite"
 OBJ_LABELS = "models/object_detection/mobilenet_ssd_v2_coco/coco_labels.txt"
-
-SCENE_URL = "/v1/vision/scene"
 SCENE_MODEL = "models/classification/dogs-vs-cats/model.tflite"
 SCENE_LABELS = "models/classification/dogs-vs-cats/labels.txt"
 
@@ -55,24 +54,19 @@ def predict_face():
         return
 
     if flask.request.files.get("image"):
-        # Open image and get bytes and size
         image_file = flask.request.files["image"]
         image_bytes = image_file.read()
-        image = Image.open(io.BytesIO(image_bytes))  # A PIL image
+        image = Image.open(io.BytesIO(image_bytes))
         image_width = image.size[0]
         image_height = image.size[1]
 
-        # Format data and send to interpreter
-        resized_image = image.resize((face_input_width, face_input_height))
+        resized_image = image.resize((face_input_width, face_input_height), Image.ANTIALIAS)
         input_data = np.expand_dims(resized_image, axis=0)
         face_interpreter.set_tensor(face_input_details[0]["index"], input_data)
 
-        # Process image and get predictions
         face_interpreter.invoke()
         boxes = face_interpreter.get_tensor(face_output_details[0]["index"])[0]
-        classes = face_interpreter.get_tensor(face_output_details[1]["index"])[
-            0
-        ]
+        classes = face_interpreter.get_tensor(face_output_details[1]["index"])[0]
         scores = face_interpreter.get_tensor(face_output_details[2]["index"])[0]
 
         faces = []
@@ -102,19 +96,16 @@ def predict_object():
         return
 
     if flask.request.files.get("image"):
-        # Open image and get bytes and size
         image_file = flask.request.files["image"]
         image_bytes = image_file.read()
-        image = Image.open(io.BytesIO(image_bytes))  # A PIL image
+        image = Image.open(io.BytesIO(image_bytes))
         image_width = image.size[0]
         image_height = image.size[1]
 
-        # Format data and send to interpreter
-        resized_image = image.resize((obj_input_width, obj_input_height))
+        resized_image = image.resize((obj_input_width, obj_input_height), Image.ANTIALIAS)
         input_data = np.expand_dims(resized_image, axis=0)
         obj_interpreter.set_tensor(obj_input_details[0]["index"], input_data)
 
-        # Process image and get predictions
         obj_interpreter.invoke()
         boxes = obj_interpreter.get_tensor(obj_output_details[0]["index"])[0]
         classes = obj_interpreter.get_tensor(obj_output_details[1]["index"])[0]
@@ -146,21 +137,13 @@ def predict_scene():
         return
 
     if flask.request.files.get("image"):
-        # Open image and get bytes and size
         image_file = flask.request.files["image"]
         image_bytes = image_file.read()
-        image = Image.open(io.BytesIO(image_bytes))  # A PIL image
-        # Format data and send to interpreter
-        resized_image = image.resize(
-            (scene_input_width, scene_input_height), Image.ANTIALIAS
-        )
+        image = Image.open(io.BytesIO(image_bytes))
+        resized_image = image.resize((scene_input_width, scene_input_height), Image.ANTIALIAS)
         results = classify_image(scene_interpreter, image=resized_image)
 
-        print(
-            f"results[0]: {results[0]}", file=sys.stderr,
-        )
         label_id, prob = results[0]
-
         data["label"] = scene_labels[label_id]
         data["confidence"] = prob
         data["success"] = True
@@ -168,9 +151,7 @@ def predict_scene():
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Flask app exposing tflite models"
-    )
+    parser = argparse.ArgumentParser(description="Flask app exposing tflite models")
     parser.add_argument("--port", default=5000, type=int, help="port number")
     args = parser.parse_args()
 
@@ -188,8 +169,8 @@ if __name__ == "__main__":
     face_interpreter.allocate_tensors()
     face_input_details = face_interpreter.get_input_details()
     face_output_details = face_interpreter.get_output_details()
-    face_input_height = face_input_details[0]["shape"][1]  # 320
-    face_input_width = face_input_details[0]["shape"][2]  # 320
+    face_input_height = face_input_details[0]["shape"][1]
+    face_input_width = face_input_details[0]["shape"][2]
 
     # Setup face detection
     scene_interpreter = tflite.Interpreter(model_path=SCENE_MODEL)
